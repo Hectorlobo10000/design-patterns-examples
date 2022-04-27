2e4ab1eafb0d56c6fa721459d769a383ecd2845f
Hectorlobo10000 <hector.lobo10000@gmail.com>
2022-04-27 17:45:27 -0600

    we created a decorator that gives ducks some new behavior

--- Patterns/PatternsOfPatterns/DuckSimulator.cs
+++ Patterns/PatternsOfPatterns/DuckSimulator.cs
@@ -6,10 +6,10 @@ namespace Patterns.PatternsOfPatterns
     {
         public void Simulate()
         {
-            var mallardDuck = new MallardDuck();
-            var redheadDuck = new RedheadDuck();
-            var duckCall = new DuckCall();
-            var rubberDuck = new RubberDuck();
+            var mallardDuck = new QuackCounter(new MallardDuck());
+            var redheadDuck = new QuackCounter(new RedheadDuck());
+            var duckCall = new QuackCounter(new DuckCall());
+            var rubberDuck = new QuackCounter(new RubberDuck());
             var gooseDuck = new GooseAdapter(new Goose());
             
             Console.WriteLine("\nDuck Simulator");
@@ -19,6 +19,8 @@ namespace Patterns.PatternsOfPatterns
             Simulate(duckCall);
             Simulate(rubberDuck);
             Simulate(gooseDuck);
+            
+            Console.WriteLine($"the ducks quacked {QuackCounter.GetQuacks()} times");
         }
 
         public void Simulate(IQuackable duck)
